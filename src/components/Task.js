9e31500ca0dfe2fb8aaa15d30d3bf68ab03dc6fc
Aniket Aryamane <aniket.aryamane@fafadiatech.com>
2023-05-23 09:50:57 +0530

    Task.js Converting to Class based component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,21 +1,24 @@
+import React from "react";
 import { FaTimes } from "react-icons/fa";
 
-const Task = ({ task, onToggle, onDelete }) => {
-    return (
-        <div
-            className={`task ${task.reminder ? "reminder" : ""}`}
-            onDoubleClick={() => onToggle(task.id)}
-        >
-            <h3>
-                {task.text}
-                <FaTimes
-                    style={{ color: "red", cursor: "pointer" }}
-                    onClick={() => onDelete(task.id)}
-                />
-            </h3>
-            <p>{task.day}</p>
-        </div>
-    );
-};
+class Task extends React.Component {
+    render() {
+        return (
+            <div
+                className={`task ${this.props.task.reminder ? "reminder" : ""}`}
+                onDoubleClick={() => this.props.onToggle(this.props.task.id)}
+            >
+                <h3>
+                    {this.props.task.text}
+                    <FaTimes
+                        style={{ color: "red", cursor: "pointer" }}
+                        onClick={() => this.props.onDelete(this.props.task.id)}
+                    />
+                </h3>
+                <p>{this.props.task.day}</p>
+            </div>
+        );
+    }
+}
 
 export default Task;
